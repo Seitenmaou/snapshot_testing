2b5fd548bfa545d41f5a22d8d7fb1d805e909884
Euri Tanaka <dev@mugendiary.com>
2022-07-10 19:51:32 -0700

    adding tests for App and GitHubCard

---
+++ src/GitHubCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const GitHubCard = () => {
+    return(
+        <div>
+            <Card>
+                <Card.Text>
+                    Seitenmaou
+                </Card.Text>
+                <Card.Body>
+                    {/* <Card.Image/> */}
+                    <Card.Text>
+                        Some dude trying to be smart.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )    
+}
+
+export default GitHubCard
